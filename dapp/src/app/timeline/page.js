fcf57fff43c7f873a423e54685d3d89941411f71
Alan-oliveir <alandeog@poli.ufrj.br>
2023-08-11 23:39:06 -0300

    change global.css and page.js

--- dapp/src/app/timeline/page.js
+++ dapp/src/app/timeline/page.js
@@ -1,9 +1,21 @@
 "use client";
 
+import Head from "next/head";
+
 export default function Timeline() {
   return (
     <>
-      <h1>Timeline</h1>
+      <Head>
+        <title>CrypTwitter | Timeline</title>
+        <meta charSet="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+      </Head>
+
+      <div className="container">
+        <div className="row">
+          <div className="layout"></div>
+        </div>
+      </div>
     </>
   );
 }
